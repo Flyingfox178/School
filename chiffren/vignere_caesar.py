25043c145a6bf74769e239b20e8d64624acb2527
Flyingfox178 <99385651+Flyingfox178@users.noreply.github.com>
2024-08-21 09:50:40 +0000

    optimiert caesar

--- chiffren/vignere_caesar.py
+++ chiffren/vignere_caesar.py
@@ -14,11 +14,7 @@ def chiffrieren_caesar(text, key):
         letternum = alph.find(text[i])
         #print("DEBUG letternum: ", letternum)
         #print("DEBUG Sum: ", letternum + keynum, len(alph))
-        if (letternum + keynum) >= len(alph):
-            #print("FALL 1")
-            newletter = alph[letternum + keynum - len(alph)]
-        else:
-            newletter = alph[letternum + keynum]
+        newletter = alph[(letternum + keynum)%len(alph)]
         cipher += newletter
     return cipher
 
