6b174f5e63cfbbe962d3993d2c4b755bd994bb18
Flyingfox178 <99385651+Flyingfox178@users.noreply.github.com>
2024-09-27 09:27:00 +0000

    Spionage anfang

---
+++ chiffren/spionage_chiffre.py
@@ -0,0 +1,19 @@
+# Spionage Chiffre
+# Joris, Alexander, Nils
+# 27.09.2024
+
+def spionage(pt, kw, knm1, knm2, knc): #pt = Plain Text/Klartext, kw = keyword/Schlüsselwort, knm = Key Numbers matrix/Schlüsselzahlen Matrix, kns = Key Number Columns/Schlüsselzahl Spalten
+    return
+
+def matrix(pt, kw, kn1, kn2): #pt = Plain Text/Klartext, kw = keyword/Schlüsselwort, kn = Key Numbers/Schlüsselzahlen
+    alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","*","/"]
+    for i in kw:
+        alph.remove(i) #Keywordbuchstaben aus dem Alphabet entfernen
+    
+    
+
+    
+                
+    
+
+matrix("lololo", "abc", 1, 4)
