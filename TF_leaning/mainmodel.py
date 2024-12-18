328110aeacd94c83459194d99c278cda84a8d7e8
Flyingfox178 <99385651+Flyingfox178@users.noreply.github.com>
2024-12-18 11:33:06 +0000

    How it works started

--- TF_leaning/mainmodel.py
+++ TF_leaning/mainmodel.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 model = tf.keras.models.load_model("my_model.keras")
 
-img0 = Image.open("example9.png").convert('L')
+img0 = Image.open("example1.png").convert('L')
 imgarr = numpy.asarray(img0)
 imgarr = imgarr/255
 print(imgarr)
